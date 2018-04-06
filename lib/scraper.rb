fb831a57827fb7ba2f6aa78f79520618d1925aec
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-04-06 21:05:10 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,11 @@ class Scraper
   
   
   def print_courses
-    
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  "
   end #print_courses
   
 end # class Scraper
